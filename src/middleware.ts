9a66c123b6dcc03a92c296b63acde8aa2cca1774
hmbtaiwtm <immaculatekriss@gmail.com>
2025-03-29 22:24:38 +0100

    cross

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,25 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-    console.log('test')
-  
-  res.headers.set('Access-Control-Allow-Origin', 'https://elaborate-douhua-170d6e.netlify.app');
-  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    const res = NextResponse.next();
 
-  if (req.method === 'OPTIONS') {
-    return new NextResponse(null, { status: 204, headers: res.headers });
-  }
+    res.headers.append("Access-Control-Allow-Credentials", "true");
+    res.headers.append("Access-Control-Allow-Origin", "https://elaborate-douhua-170d6e.netlify.app");
+    res.headers.append("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT,OPTIONS");
+    res.headers.append(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
+    );
 
-  return res;
+
+    if (req.method === "OPTIONS") {
+        return new NextResponse(null, { status: 204, headers: res.headers });
+    }
+
+    return res;
 }
 
 export const config = {
-  matcher: ['/api/:path*'],
+    matcher: "/api/:path*",
 };
